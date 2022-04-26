cdf3b474c4d167f93122ee03fe2fb23dc16eedf6
shannonasor <shannonreed@asordevelopment.com>
2022-04-26 12:01:19 -0700

    added landing page component

--- keya-graduation/src/App.js
+++ keya-graduation/src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './img/2022icon.png';
 import Keya from './img/keya.png';
 import Keya1 from './img/keya1.png';
@@ -5,48 +6,12 @@ import Keya2 from './img/keya2.png';
 import Keya3 from './img/keya3.png';
 import './App.css';
 
+import Landing from './Landing/Landing.js';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-header-divs middle">
-        <h1 className="Main-name Main-text">
-          Shakeya Bell
-        </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="Main-text Main-graduate">
-          Graduate
-        </h1>
-        </div>
-
-        <div className=" App-header-divs right">
-        <div>
-        <figure>
-        <img src={Keya2} class="img-fill" alt="keya1"/>
-        <figcaption>Caption One</figcaption>
-        </figure>
-        <figure>
-        <img src={Keya1} class="img-fill" alt="keya1"/>
-        <figcaption>Caption One</figcaption>
-        </figure>
-        </div>
-        <div>
-        <figure>
-        <img src={Keya3} class="img-fill" alt="keya1"/>
-        <figcaption>Caption One</figcaption>
-        </figure>
-        <figure>
-        <img src={Keya} class="img-fill" alt="keya1"/>
-        <figcaption>Caption One</figcaption>
-        </figure>
-        </div>
-        </div>
-        
-        {/*2022 Icon attribute*/}cd
-        {/* <a href="https://www.flaticon.com/free-icons/2022" title="2022 icons">2022 icons created by Vector Squad - Flaticon</a> */}
-      </header>
-
-      
+    <div>
+    <Landing/>
     </div>
   );
 }
--- keya-graduation/src/Landing/Landing.js
+++ keya-graduation/src/Landing/Landing.js
@@ -1,12 +1,57 @@
-import logo from './2022icon.png';
-import Keya from './keya.png';
-import Keya1 from './keya1.png';
-import Keya2 from './keya2.png';
-import Keya3 from './keya3.png';
+
+import logo from '../img/2022icon.png';
+import Keya from '../img/keya.png';
+import Keya1 from '../img/keya1.png';
+import Keya2 from '../img/keya2.png';
+import Keya3 from '../img/keya3.png';
+import './landing.css';
 
 
 
 function Landing () {
+    return (
+    <div className="App">
+    <header className="App-header">
+      <div className="App-header-divs middle">
+      <h1 className="Main-name Main-text">
+        Shakeya Bell
+      </h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="Main-text Main-graduate">
+        Graduate
+      </h1>
+      </div>
+
+      <div className=" App-header-divs right">
+      <div>
+      <figure>
+      <img src={Keya2} class="img-fill" alt="keya1"/>
+      <figcaption>Caption One</figcaption>
+      </figure>
+      <figure>
+      <img src={Keya1} class="img-fill" alt="keya1"/>
+      <figcaption>Caption One</figcaption>
+      </figure>
+      </div>
+      <div>
+      <figure>
+      <img src={Keya3} class="img-fill" alt="keya1"/>
+      <figcaption>Caption One</figcaption>
+      </figure>
+      <figure>
+      <img src={Keya} class="img-fill" alt="keya1"/>
+      <figcaption>Caption One</figcaption>
+      </figure>
+      </div>
+      </div>
+      
+      {/*2022 Icon attribute*/}
+      {/* <a href="https://www.flaticon.com/free-icons/2022" title="2022 icons">2022 icons created by Vector Squad - Flaticon</a> */}
+    </header>
+
+    
+  </div>
+    )
 
 }
 
