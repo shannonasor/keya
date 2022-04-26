588fcc4ab1e40873207ae953e22812b84868467c
shannonasor <shannonreed@asordevelopment.com>
2022-04-26 11:42:39 -0700

    added landing page

--- keya-graduation/src/App.js
+++ keya-graduation/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import logo from './img/2022icon.png';
+import Keya from './img/keya.png';
+import Keya1 from './img/keya1.png';
+import Keya2 from './img/keya2.png';
+import Keya3 from './img/keya3.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="App-header-divs middle">
+        <h1 className="Main-name Main-text">
+          Shakeya Bell
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="Main-text Main-graduate">
+          Graduate
+        </h1>
+        </div>
+
+        <div className=" App-header-divs right">
+        <div>
+        <figure>
+        <img src={Keya2} class="img-fill" alt="keya1"/>
+        <figcaption>Caption One</figcaption>
+        </figure>
+        <figure>
+        <img src={Keya1} class="img-fill" alt="keya1"/>
+        <figcaption>Caption One</figcaption>
+        </figure>
+        </div>
+        <div>
+        <figure>
+        <img src={Keya3} class="img-fill" alt="keya1"/>
+        <figcaption>Caption One</figcaption>
+        </figure>
+        <figure>
+        <img src={Keya} class="img-fill" alt="keya1"/>
+        <figcaption>Caption One</figcaption>
+        </figure>
+        </div>
+        </div>
+        
+        {/*2022 Icon attribute*/}cd
+        {/* <a href="https://www.flaticon.com/free-icons/2022" title="2022 icons">2022 icons created by Vector Squad - Flaticon</a> */}
       </header>
+
+      
     </div>
   );
 }
---
+++ keya-graduation/src/Landing/Landing.js
@@ -0,0 +1,13 @@
+import logo from './2022icon.png';
+import Keya from './keya.png';
+import Keya1 from './keya1.png';
+import Keya2 from './keya2.png';
+import Keya3 from './keya3.png';
+
+
+
+function Landing () {
+
+}
+
+export default Landing
